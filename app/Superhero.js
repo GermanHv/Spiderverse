f12d4dcb609bfbf54c27568fab3a1b233f2794d3
GermanHv <ghove7@gmail.com>
2022-04-20 20:34:50 -0500

    se añade función getInfo()

--- app/Superhero.js
+++ app/Superhero.js
@@ -1,9 +1,18 @@
 class Superhero {
-    constructor(name, heroName, actor){
+    constructor(name, heroName, actor, message){
         this.name = name
         this.heroName = heroName
         this.actor = actor
+        this.message = message
     }
+
+    getInfo(){
+
+       return `Mensaje: ${this.message}`
+
+    }
+    
 }
 
 module.exports = Superhero
+
